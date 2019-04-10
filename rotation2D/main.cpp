0a7eb2699ee275c92879514ae126cc289bf75a80
jmendesf <jmendesforte@gmail.com>
2019-04-10 10:20:50 +0200

    add dt image representation and image/dt addition

--- rotation2D/main.cpp
+++ rotation2D/main.cpp
@@ -20,9 +20,17 @@ using namespace std;
 using namespace DGtal;
 
 // 2D image definition
-typedef ImageContainerBySTLVector<Z2i::Domain, unsigned char>  Image;
+typedef ImageContainerBySTLVector<Z2i::Domain, int>  Image;
 // Grayscale mapping
-typedef GrayscaleColorMap<unsigned char> Gray;
+typedef GrayscaleColorMap<int> Gray;
+
+// Create a binarizer
+typedef functors::IntervalForegroundPredicate<Image> Binarizer;  
+
+// Create and apply a distance transform to the binary image with the L2 norm
+// Def L1 norm aswell
+typedef DistanceTransformation<Z2i::Space, Binarizer, Z2i::L2Metric> DTL2;
+typedef DistanceTransformation<Z2i::Space, Binarizer, Z2i::L1Metric> DTL1;
 
 // Print if wrong args
 void usage()
@@ -38,50 +46,110 @@ void inverseImage(Image& image)
       image.setValue({x,y}, 255 - int(image.operator()({x,y})));
 }
 
-void processImage(Image& image, Image& imInv)
+// Returns an image corresponding to the given DT
+Image createImageFromDT(DTL1 dtl1, int maxValue)
+{
+  Image im(dtl1.domain());
+  int step = maxValue != 0 ? 255 / maxValue : 0;
+  int value;
+
+  for(int y = 0; y < im.domain().upperBound()[1]; ++y)
+    for(int x = 0; x < im.domain().upperBound()[0]; ++x)
+    {
+      value = dtl1.operator()({x,y});
+      im.setValue({x,y}, value * step);
+    }
+  return im;
+}
+
+// Returns the addition of two images
+Image addImages(Image im1, Image im2)
+{
+  Image im((im1.domain().size() > im2.domain().size() ? im1.domain() : im2.domain()));
+  int value;
+
+  for(int y = 0; y < im.domain().upperBound()[1]; ++y)
+    for(int x = 0; x < im.domain().upperBound()[0]; ++x)
+    {
+      value = im1.operator()({x,y}) + im2.operator()({x,y});
+      if(value > 255)
+        value = 255;
+
+      im.setValue({x, y}, value);
+    }
+  return im;
+}
+
+// Returns the addition of two DT in the form of an image
+Image addImages(DTL1 dtl1Im1, DTL1 dtl1Im2)
+{
+  Image im((dtl1Im1.domain().size() > dtl1Im2.domain().size() ? dtl1Im1.domain() : dtl1Im2.domain()));
+
+  DTL1::Value maxDT2 = (*std::max_element(dtl1Im1.constRange().begin(), dtl1Im1.constRange().end()));
+  DTL1::Value maxDT1 = (*std::max_element(dtl1Im2.constRange().begin(), dtl1Im2.constRange().end()));
+
+  int step = 255 / maxDT2;
+  int step2 = 255 / maxDT1;
+
+  for(int y = 0; y < im.domain().upperBound()[1]; ++y)
+  {
+    for(int x = 0; x < im.domain().upperBound()[0]; ++x)
+    {
+      int value = dtl1Im1.operator()({x,y});
+      int value2 = dtl1Im2.operator()({x,y});
+      
+      im.setValue({x,y}, max(value * step, value2 * step2));
+    }
+  }
+  return im;
+}
+
+void processImage(Image& image)
 {
   // Create board and set its size
   Board2D board;
   board << image.domain();
   board.clear();
 
-  // import image to the board
+  Image imInv = image;
+
+  // Import image to the board
   Display2DFactory::drawImage<Gray>(board, image, (unsigned char)0, (unsigned char)255);
 
-  // Create a binarizer
-  typedef functors::IntervalForegroundPredicate<Image> Binarizer;
-
+  // Get the inverse
   inverseImage(imInv);
 
   // Threshold the image
   Binarizer b(image, 1, 135);
   Binarizer bInv(imInv, 1, 135);
 
-  // Create and apply a distance transform to the binary image with the L2 norm
-  // Def L1 norm aswell
-  typedef DistanceTransformation<Z2i::Space, Binarizer, Z2i::L2Metric> DTL2;
-  typedef DistanceTransformation<Z2i::Space, Binarizer, Z2i::L1Metric> DTL1;
-
-  DTL2 dtl2(&image.domain(), &b, &Z2i::l2Metric);
-  DTL1 dtl1(&imInv.domain(), &bInv, &Z2i::l1Metric);
+  // DTL2 dtl2(&image.domain(), &b, &Z2i::l2Metric);
+  DTL1 dtl1(&image.domain(), &b, &Z2i::l1Metric);
+  DTL1 dtl1Inv(&imInv.domain(), &bInv, &Z2i::l1Metric);
 
   // Compute the max value of the DT
-  DTL2::Value maxDT2 = (*std::max_element(dtl2.constRange().begin(), dtl2.constRange().end()));
-  DTL1::Value maxDT1 = (*std::max_element(dtl1.constRange().begin(), dtl1.constRange().end()));
+  DTL1::Value maxDT2 = (*std::max_element(dtl1.constRange().begin(), dtl1.constRange().end()));
+  DTL1::Value maxDT1 = (*std::max_element(dtl1Inv.constRange().begin(), dtl1Inv.constRange().end()));
 
   // Hue color map
-  typedef DGtal::HueShadeColorMap<DTL2::Value,2> HueTwice;
+  // typedef DGtal::HueShadeColorMap<DTL1::Value,2> HueTwice;
 
-  // Clear board
-  board.clear();
+  // Create GS DT images
+  Image imGS = createImageFromDT(dtl1, maxDT2);
+  Image imInvGS = createImageFromDT(dtl1Inv, maxDT1);
+  // Add both DT
+  Image imAddDTL = addImages(dtl1, dtl1Inv);
 
-  // Import GS DT image to the board
-  // Save as output
-  Display2DFactory::drawImage<Gray>(board, dtl2, (DTL2::Value)0, (DTL2::Value)maxDT2);
-  board.saveEPS("../output/gray_scale.eps");
+  // Save output 
   board.clear();
-  Display2DFactory::drawImage<Gray>(board, dtl1, (DTL1::Value)0, (DTL1::Value)maxDT1);
-  board.saveEPS("../output/gray_scale_inv.eps");
+  Display2DFactory::drawImage<Gray>(board, imAddDTL, 0, 255);
+  board.saveEPS("../output/im_add_DT");
+  board.clear();
+  Display2DFactory::drawImage<Gray>(board, imGS, 0, 255);
+  board.saveEPS("../output/im_GS_DT.eps");
+  board.clear();
+  Display2DFactory::drawImage<Gray>(board, imInvGS, 0, 255);
+  board.saveEPS("../output/im_inv_GS_DT");
 
   // Create a set to put the DT in
   // Set for the inverse of the image aswell
@@ -89,7 +157,7 @@ void processImage(Image& image, Image& imInv)
   Z2i::DigitalSet setInv(imInv.domain());
   DigitalSetInserter<Z2i::DigitalSet> inserter(set);
   DigitalSetInserter<Z2i::DigitalSet> inserterInv(setInv);
-  DGtal::setFromImage(dtl2, inserter, 1, 135);
+  DGtal::setFromImage(dtl1, inserter, 1, 135);
   DGtal::setFromImage(imInv, inserterInv, 1, 135);
 
   // Save both sets
@@ -98,8 +166,7 @@ void processImage(Image& image, Image& imInv)
   board.saveEPS("../output/set.eps");
   board.clear();
   board << setInv.domain() << setInv;
-  board.saveEPS("../output/setInv.eps");
-
+  board.saveEPS("../output/set_Inv.eps");
 }
 
 
@@ -114,8 +181,7 @@ int main(int argc, char** argv)
 
   // Load image
   Image image = PGMReader<Image>::importPGM(argv[1]);
-  Image imInv = PGMReader<Image>::importPGM(argv[1]);
-  processImage(image, imInv);
+  processImage(image);
 
   return 0;
 }
