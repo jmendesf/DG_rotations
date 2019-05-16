a07165280ec3c581790cad67499b4ed709c119cb
jmendesf <jmendesforte@gmail.com>
2019-05-16 10:14:15 +0200

    add nearest neighbor 3D rotation

--- rotation3D/src/main.cpp
+++ rotation3D/src/main.cpp
@@ -29,6 +29,8 @@
 using std::cout;
 using std::endl;
 using std::string;
+using std::min;
+using std::max;
 using namespace DGtal;
 using namespace DGtal::Z3i;
 namespace po = boost::program_options;
@@ -39,7 +41,7 @@ typedef DistanceTransformation<Z3i::Space, Predicate, Z3i::L2Metric> DTL2;
 
 void usage() {
     cout << "usage: ./rotation3D angle aX aY aZ visualisation" << endl;
-    cout << "visualisation: shape, dt" << endl;
+    cout << "visualisation: shape, dt, rot" << endl;
 }
 
 void findExtrema(DTL2 dtL2, float &min, float &max) {
@@ -106,7 +108,7 @@ void addDTImages(Image im1, Image im2, Image &dst) {
 }
 
 // for debugging
-void printMatrix(float matrix[3][3]) {
+void printMatrix(double matrix[3][3]) {
     for (int i = 0; i < 3; i++) {
         for (int y = 0; y < 3; y++)
             cout << matrix[i][y] << " ";
@@ -114,7 +116,8 @@ void printMatrix(float matrix[3][3]) {
     }
 }
 
-void computeRotationMatrix(float v1, float v2, float v3, float matrix[3][3], float angle) {
+void computeRotationMatrix(double v1, double v2, double v3, double matrix[3][3], double angle) {
+
     // Using Rodrigues' formula:
     // R = I + sin(angle) * A + (1 - cos(angle)) * A^2
     // With R the rotation matrix,
@@ -124,10 +127,14 @@ void computeRotationMatrix(float v1, float v2, float v3, float matrix[3][3], flo
     // A = ( v3   0   -v1)
     //     (-v2   v1   0 )
 
-    float A[3][3] = {{0,   -v3, v2},
+    double w = v1 * v1 + v2 * v2 + v3 * v3;
+
+    // A matrix
+    double A[3][3] = {{0,   -v3, v2},
                      {v3,  0,   -v1},
                      {-v2, v1,  0}};
-    float ASquared[3][3] =
+    // A matrix squared
+    double ASquared[3][3] =
             {
                     {
                             A[0][0] * A[0][0] + A[0][1] * A[1][0] + A[0][2] * A[2][0],
@@ -145,32 +152,50 @@ void computeRotationMatrix(float v1, float v2, float v3, float matrix[3][3], flo
                             A[2][0] * A[0][2] + A[2][1] * A[1][2] + A[2][2] * A[2][2]
                     }
             };
+
+    for(int i = 0; i < 3; ++i)
+    {
+        for(int y = 0; y < 3; ++y)
+        {
+            ASquared[i][y] /= w;
+            A[i][y] /= std::sqrt(w);
+        }
+    }
+
     float cosCoeff = 1 - cos(angle);
 
-    for (int i = 0; i < 3; i++)
-    {
-        for (int y = 0; y < 3; y++)
-        {
+    // first operations
+    for (int i = 0; i < 3; i++) {
+        for (int y = 0; y < 3; y++) {
             A[i][y] *= sin(angle);
             ASquared[i][y] *= cosCoeff;
         }
     }
 
+    // add identity matrix
     A[0][0] += 1;
     A[1][1] += 1;
     A[2][2] += 1;
 
-    for(int i = 0; i < 3; i++)
-    {
-        for(int y = 0; y < 3; y++)
-        {
+    // add both matrices to the final matrix
+    for (int i = 0; i < 3; i++) {
+        for (int y = 0; y < 3; y++) {
             matrix[i][y] = A[i][y] + ASquared[i][y];
         }
     }
 }
 
+PointVector<3, double> computeRotation(PointVector<3, int> p, double rotationMatrix[3][3]) {
+    PointVector<3, double> result;
+    int x = p[0], y = p[1], z = p[2];
+    result[0] = x * rotationMatrix[0][0] + y * rotationMatrix[0][1] + z * rotationMatrix[0][2];
+    result[1] = x * rotationMatrix[1][0] + y * rotationMatrix[1][1] + z * rotationMatrix[1][2];
+    result[2] = x * rotationMatrix[2][0] + y * rotationMatrix[2][1] + z * rotationMatrix[2][2];
 
-void rotateBackward(Image image, Image &dst, float angle, float v1, float v2, float v3) {
+    return result;
+}
+
+Image rotateBackward(Image image, double angle, double v1, double v2, double v3) {
     int maxX = image.domain().upperBound()[0];
     int maxY = image.domain().upperBound()[1];
     int maxZ = image.domain().upperBound()[2];
@@ -182,10 +207,89 @@ void rotateBackward(Image image, Image &dst, float angle, float v1, float v2, fl
                                (maxY - minY) / 2,
                                (maxZ - minZ) / 2);
 
-    float matrix[3][3];
-    cout << "-- Computing Rodrigues' rotation matrix ";
-    computeRotationMatrix(v1, v2, v3, matrix, angle);
-    cout << "-done." << endl;
+    double matrixForward[3][3];
+    double matrixBackward[3][3];
+
+    cout << "-- Computing Rodrigues' rotation matrices " << endl;
+    cout << "     - Forward";
+    computeRotationMatrix(v1, v2, v3, matrixForward, angle);
+    cout << " - done." << endl;
+    cout << "     - Backward";
+    computeRotationMatrix(v1, v2, v3, matrixBackward, -angle);
+    cout << " - done." << endl;
+
+    cout << endl;
+    printMatrix(matrixForward);
+    cout << endl;
+    printMatrix(matrixBackward);
+    cout << endl;
+
+    cout << "-- Determining rotated domain boundaries by forward rotation ";
+    PointVector<3, double> p000 = computeRotation({minX, minY, minZ}, matrixForward);
+    PointVector<3, double> p100 = computeRotation({maxX, minY, minZ}, matrixForward);
+    PointVector<3, double> p010 = computeRotation({minX, maxY, minZ}, matrixForward);
+    PointVector<3, double> p110 = computeRotation({maxX, maxY, minZ}, matrixForward);
+    PointVector<3, double> p001 = computeRotation({minX, minY, maxZ}, matrixForward);
+    PointVector<3, double> p101 = computeRotation({maxX, minY, maxZ}, matrixForward);
+    PointVector<3, double> p111 = computeRotation({maxX, maxY, maxZ}, matrixForward);
+    PointVector<3, double> p011 = computeRotation({minX, maxY, maxZ}, matrixForward);
+
+    int minXR, minYR, minZR;
+    int maxXR, maxYR, maxZR;
+
+    minXR = min(min(min(p000[0], p100[0]), min(p010[0], p110[0])), min(min(p001[0], p101[0]), min(p111[0], p011[0])));
+    minYR = min(min(min(p000[1], p100[1]), min(p010[1], p110[1])), min(min(p001[1], p101[1]), min(p111[1], p011[1])));
+    minZR = min(min(min(p000[2], p100[2]), min(p010[2], p110[2])), min(min(p001[2], p101[2]), min(p111[2], p011[2])));
+    cout << minX << " " << minY << " " << minZ << endl;
+    cout << minXR << ", "<< minYR << ", " << minZR << endl;
+
+    maxXR = max(max(max(p000[0], p100[0]), max(p010[0], p110[0])), max(max(p001[0], p101[0]), max(p111[0], p011[0])));
+    maxYR = max(max(max(p000[1], p100[1]), max(p010[1], p110[1])), max(max(p001[1], p101[1]), max(p111[1], p011[1])));
+    maxZR = max(max(max(p000[2], p100[2]), max(p010[2], p110[2])), max(max(p001[2], p101[2]), max(p111[2], p011[2])));
+
+    cout << maxX << " " << maxY << " " << maxZ << endl;
+    cout << maxXR << ", "<< maxYR << ", " << maxZR << endl;
+
+    PointVector<3, int> lowerBound = {minXR, minYR, minZR};
+    PointVector<3, int> upperBound = {maxXR, maxYR, maxZR};
+    Z3i::Domain newDomain(lowerBound, upperBound);
+    Image imRot(newDomain);
+    cout << "- done." << endl;
+
+    cout << "-- Computing rotation " << endl;
+    for (int z = newDomain.lowerBound()[2]; z <= newDomain.upperBound()[2]; ++z) {
+        for (int y = newDomain.lowerBound()[1]; y <= newDomain.upperBound()[1]; ++y) {
+            for (int x = newDomain.lowerBound()[0]; x <= newDomain.upperBound()[0]; ++x) {
+                PointVector<3, double> backP = computeRotation({x, y, z}, matrixBackward);
+                int backX = round(backP[0]);
+                int backY = round(backP[1]);
+                int backZ = round(backP[2]);
+
+                if (backX <= minX || backY <= minY || backZ <= minZ)
+                {
+                    imRot.setValue({x, y, z}, 1);
+                    continue;
+                }
+
+                if (backX >= maxX || backY >= maxY || backZ >= maxZ)
+                {
+                    imRot.setValue({x, y, z}, 1);
+                    continue;
+                }
+
+                imRot.setValue({x, y, z}, image.operator()({backX, backY, backZ}));
+            }
+        }
+    }
+    return imRot;
+}
+
+void thresholdDTImage(Image src, Image &dst) {
+    for (Z3i::Domain::ConstIterator it = src.domain().begin(), itend = src.domain().end();
+         it != itend;
+         ++it) {
+        dst.setValue(*it, src(*it) > 0 ? 0 : 255);
+    }
 }
 
 int main(int argc, char **argv) {
@@ -193,10 +297,10 @@ int main(int argc, char **argv) {
     float angle;
 
     if (argc == 6) {
-        angle = stof(argv[1]);
-        vecRotation[0] = stof(argv[2]);
-        vecRotation[1] = stof(argv[3]);
-        vecRotation[2] = stof(argv[4]);
+        angle = stod(argv[1]);
+        vecRotation[0] = stod(argv[2]);
+        vecRotation[1] = stod(argv[3]);
+        vecRotation[2] = stod(argv[4]);
     } else {
         usage();
         return 0;
@@ -257,7 +361,11 @@ int main(int argc, char **argv) {
     addDTImages(dtL2Im, dtL2ImInv, DTAddIm);
     cout << "- done." << endl;
 
-    GradientColorMap<long> gradient(-max - 1, maxInv + 1);
+    Image imRot = rotateBackward(DTAddIm, angle, vecRotation[0], vecRotation[1], vecRotation[2]);
+    Image threshImRot(imRot.domain());
+    thresholdDTImage(imRot, threshImRot);
+
+    GradientColorMap<long> gradient(0, 255);
     gradient.addColor(Color::Red);
     gradient.addColor(Color::Yellow);
     gradient.addColor(Color::Green);
@@ -268,13 +376,12 @@ int main(int argc, char **argv) {
     float transp = 20.;
 
     viewer << SetMode3D((*(domain.begin())).className(), "Paving");
-
     if (strcmp(argv[5], "dt") == 0) {
         for (Z3i::Domain::ConstIterator it = domain.begin(), itend = domain.end();
              it != itend;
              ++it) {
 
-            double valDist = DTAddIm((*it));
+            double valDist = imRot((*it));
             if (valDist > -max) {
                 if (valDist < 0)
                     valDist = maxInv - abs(valDist);
@@ -305,13 +412,27 @@ int main(int argc, char **argv) {
                 viewer << *it;
             }
         }
+    } else if (strcmp(argv[5], "rot") == 0) {
+        for (Z3i::Domain::ConstIterator it = imRot.domain().begin(), itend = imRot.domain().end();
+             it != itend;
+             ++it) {
+
+            double valDist = threshImRot((*it));
+            if (valDist > 254) {
+                Color c = gradient(valDist);
+                viewer << CustomColors3D(Color((float) (c.red()),
+                                               (float) (c.green()),
+                                               (float) (c.blue(), 255)),
+                                         Color((float) (c.red()),
+                                               (float) (c.green()),
+                                               (float) (c.blue()), 255));
+                viewer << *it;
+            }
+        }
     } else {
         cout << "Unknown argument " << argv[5] << ". Exiting." << endl;
         return 0;
     }
-
-    rotateBackward(image, image, angle, vecRotation[0], vecRotation[1], vecRotation[2]);
-
     viewer << Viewer3D<>::updateDisplay;
     viewer.show();
 
